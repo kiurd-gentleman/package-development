7f03721f393bf0010a63b7f11a105790f522e565
kiurd-gentelman <k.r.imtiaz@gmail.com>
2023-02-17 02:54:53 +0600

    initial commit

---
+++ unique-slug/src/UniqueSlugServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Krimt\UniqueSlug;
+
+use Illuminate\Support\ServiceProvider;
+
+class UniqueSlugServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
